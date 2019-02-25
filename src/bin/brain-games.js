8ce2c64a2b487eafc23cdc466f58e201fb5ecd8c
artem <borman306@yandex.ru>
2019-02-26 01:15:51 +0300

    move all logic to index.js

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,5 +1,4 @@
 #! /usr/bin/env node
 import { greetUser } from '..';
 
-console.log('Welcome to the Brain Games!');
 greetUser();
--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 import readlineSync from 'readline-sync';
 
-const getName = () => readlineSync.question('May I have your name? ');
+const getName = () => {
+  console.log('Welcome to the Brain Games!');
+  return readlineSync.question('May I have your name? ');
+};
+
 export const greetUser = () => console.log(`Hello, ${getName()}!`);
