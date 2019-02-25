8512c449d78d55e4b7a63b90279fdfe90aeb74f6
artem <borman306@yandex.ru>
2019-02-26 01:20:08 +0300

    chore index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import readlineSync from 'readline-sync';
 
-const getName = () => {
-  console.log('Welcome to the Brain Games!');
-  return readlineSync.question('May I have your name? ');
-};
+const getName = () => readlineSync.question('May I have your name? ');
 
-export const greetUser = () => console.log(`Hello, ${getName()}!`);
+export const greetUser = () => {
+  console.log('Welcome to the Brain Games!'); 
+  console.log(`Hello, ${getName()}!`);
+};
