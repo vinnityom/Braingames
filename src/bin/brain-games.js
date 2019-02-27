d945dcba1b72b53cc7f0e7fe97ac852f227eab97
artem <borman306@yandex.ru>
2019-02-27 23:05:34 +0300

    add engine (index.js)

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,4 +1,4 @@
 #! /usr/bin/env node
-import greetUser from '..';
+import { greetUser } from '..';
 
 greetUser();
--- src/index.js
+++ src/index.js
@@ -1,8 +1,40 @@
 import readlineSync from 'readline-sync';
 
 const getName = () => readlineSync.question('May I have your name? ');
+export const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min)) + min;
 
-export default () => {
+export const greetUser = (description = '') => {
   console.log('Welcome to the Brain Games!');
-  console.log(`Hello, ${getName()}!`);
+  console.log(description);
+  const name = getName();
+  console.log(`Hello ${name}!`);
+  return name;
+};
+
+export const beginGame = (getGameData, description) => {
+  const user = greetUser(description);
+  const numberOfRounds = 3;
+
+  const startRound = (roundCounter) => {
+    const { question, answer: correctAnswer } = getGameData();
+    console.log(`Question: ${question}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+    if (userAnswer !== correctAnswer) {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+      return roundCounter;
+    }
+    console.log('Correct!');
+    if (roundCounter < numberOfRounds) {
+      return startRound(roundCounter + 1);
+    }
+    return roundCounter;
+  };
+
+  const playedRounds = startRound(1);
+
+  if (playedRounds === numberOfRounds) {
+    console.log(`Congratulations, ${user}!`);
+  } else {
+    console.log(`Let's try again, ${user}!`);
+  }
 };
