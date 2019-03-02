f23f076c6c6949c9ce49d304e45054ccd3840c48
artem <borman306@yandex.ru>
2019-03-02 13:11:51 +0300

    fix negayive numbers bug (prime.js)

--- src/games/prime.js
+++ src/games/prime.js
@@ -3,22 +3,26 @@ import getRandomInt from '../utils';
 
 const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 const isPrime = (num) => {
-  const inner = (devisor) => {
-    if (devisor === num) {
+  if (num < 2) {
+    return false;
+  }
+
+  const isDivisorOfNum = (divisor) => {
+    if (divisor > num / 2) {
       return true;
     }
-    if (num % devisor === 0) {
+    if (num % divisor === 0) {
       return false;
     }
 
-    return inner(devisor + 1);
+    return isDivisorOfNum(divisor + 1);
   };
 
-  return inner(2);
+  return isDivisorOfNum(2);
 };
 
 const generate = () => {
-  const number = getRandomInt(1, 100);
+  const number = getRandomInt(-100, 100);
   const answer = isPrime(number) ? 'yes' : 'no';
 
   return {
